fdbaf289eb7e8d165529ba9f79372a71ef499e4a
Gabo <gleyvamerino@smu.edu>
2017-05-31 13:34:52 -0500

    Sign Up view started

--- Moon/LoginViewController.swift
+++ Moon/LoginViewController.swift
@@ -11,16 +11,23 @@ import Material
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: TextField!
+    @IBOutlet weak var passwordTextField: TextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        setUpLoginView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let searchController = SearchBarViewController(rootViewController: MasterViewController.instantiateFromStoryboard())
-        self.present(searchController, animated: true, completion: nil)
+//        let searchController = SearchBarViewController(rootViewController: MasterViewController.instantiateFromStoryboard())
+//        self.present(searchController, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,4 +35,32 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpLoginView() {
+        
+        //Email Text Field Set Up
+        emailTextField.dividerNormalColor = .white
+        emailTextField.dividerActiveColor = .white
+        emailTextField.placeholder = "email"
+        emailTextField.placeholderActiveColor = .white
+        emailTextField.placeholderNormalColor = .lightGray
+        emailTextField.textColor = .white
+        
+        //Password Text Field Set Up
+        passwordTextField.dividerNormalColor = .white
+        passwordTextField.dividerActiveColor = .white
+        passwordTextField.placeholder = "password"
+        passwordTextField.placeholderActiveColor = .white
+        passwordTextField.placeholderNormalColor = .lightGray
+        passwordTextField.textColor = .white
+        
+        //Login Button Set Up
+        loginButton.backgroundColor = .white
+        loginButton.setTitleColor(.moonBlue, for: .normal)
+        loginButton.layer.cornerRadius = 5
+        
+        //Sign Up Button Set Up
+        signUpButton.backgroundColor = .white
+        signUpButton.setTitleColor(.moonGreen, for: .normal)
+        signUpButton.layer.cornerRadius = 5
+    }
 }
